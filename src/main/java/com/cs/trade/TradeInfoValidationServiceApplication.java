5380cb887e2d21f522567d17f1a12523ebc825d1
Tomasz Szymanski <szymaniaq@gmail.com>
2018-07-06 00:23:55 +0200

    adding rest template as simple client

--- src/main/java/com/cs/trade/TradeInfoValidationServiceApplication.java
+++ src/main/java/com/cs/trade/TradeInfoValidationServiceApplication.java
@@ -1,12 +1,46 @@
 package com.cs.trade;
 
+import com.cs.trade.info.TradeInfo;
+import com.cs.trade.validation.result.ValidationResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class TradeInfoValidationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TradeInfoValidationServiceApplication.class, args);
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(TradeInfoValidationServiceApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeInfoValidationServiceApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            TradeInfo tradeInfo = new TradeInfo();
+            tradeInfo.setCcyPair("EURUSD");
+            tradeInfo.setCustomer("PLUTON");
+            tradeInfo.setValueDate(LocalDate.parse("2018-07-04"));
+            tradeInfo.setTradeDate(LocalDate.parse("2018-02-01"));
+
+            HttpEntity<TradeInfo> request = new HttpEntity<>(tradeInfo);
+            String url = "http://localhost:8001/trade-info/validate";
+            ValidationResult result = restTemplate.postForObject(url, request, ValidationResult.class);
+            LOGGER.debug(result.toString());
+        };
+    }
 }
